f1352736d5a3b64a7235fd9d07205f10e3061c6f
Orazmyrat-Hojamyradov <yakupyakupbow197@gmail.com>
2024-09-25 16:58:49 +0500

    deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
 import db from "@/db/db";
-import nextAuth, { AuthOptions } from "next-auth";
+import nextAuth from "next-auth/next";
+import { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import NextAuth from "next-auth/next";
 
 const GITHUB_ID = "Ov23liKzd4OZFPD4tsKB";
 const GITHUB_SECRET = "504ee2796b50f840b8af74e885ecc0cf4e76490f";
@@ -72,5 +72,4 @@ export const authOptions: AuthOptions = {
 
 const handler = nextAuth(authOptions);
 
-export const GET = NextAuth(authOptions);
-export const POST = NextAuth(authOptions);
+export { handler as GET, handler as POST };
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { NextApiRequest, NextApiResponse } from "next";
 import db from "@/db/db";
 
@@ -5,24 +7,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    if (req.method === "GET") {
-      const products = await db.product.findMany({
-        where: { isAvailableForPurchase: true },
-      });
-
-      // If products array is empty, return a message or empty array
-      if (!products || products.length === 0) {
-        return res.status(200).json({ message: "No products available" });
-      }
-
-      res.status(200).json(products);
-    } else {
-      res.status(405).json({ message: "Method not allowed" });
-    }
-  } catch (error) {
-    // Handle any potential errors
-    console.error("Failed to fetch products:", error);
-    res.status(500).json({ message: "Internal Server Error" });
+  if (req.method === "GET") {
+    const products = await db.product.findMany({
+      where: { isAvailableForPurchase: true },
+    });
+    res.status(200).json(products);
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
   }
 }
