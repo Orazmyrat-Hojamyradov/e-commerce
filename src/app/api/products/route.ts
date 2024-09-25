cc7b4ad9f73ec46ff774da11d67be8ccfccf871a
Orazmyrat-Hojamyradov <yakupyakupbow197@gmail.com>
2024-09-25 16:30:10 +0500

    deploy

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,3 @@
-"use server";
-
 import { NextApiRequest, NextApiResponse } from "next";
 import db from "@/db/db";
 
@@ -7,13 +5,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "GET") {
-    const products =
-      (await db.product.findMany({
+  try {
+    if (req.method === "GET") {
+      const products = await db.product.findMany({
         where: { isAvailableForPurchase: true },
-      })) || [];
-    res.status(200).json(products);
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
+      });
+
+      // If products array is empty, return a message or empty array
+      if (!products || products.length === 0) {
+        return res.status(200).json({ message: "No products available" });
+      }
+
+      res.status(200).json(products);
+    } else {
+      res.status(405).json({ message: "Method not allowed" });
+    }
+  } catch (error) {
+    // Handle any potential errors
+    console.error("Failed to fetch products:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
